e4c7cf7d1e34fc2712522151bf5615c267222a64
MatiasFernandez <mfernandez@10pines.com>
2022-01-28 16:31:49 -0300

    Extend String#but_last to receive a number of characters to be removed

--- lib/ruby_on_asteroids/core_ext/string/accessing.rb
+++ lib/ruby_on_asteroids/core_ext/string/accessing.rb
@@ -5,10 +5,11 @@ module RubyOnAsteroids
     module String
       # Useful extensions to String class related to accessing the string content
       module Accessing
-        # Returns a new String object without the last character of the original one.
-        # When the String is empty, returns an empty String
-        def but_last
-          self[0..-2]
+        # Returns a copy of the original string object with the last `num_of_characters` being removed.
+        # If `num_of_characters` is not provided, it defaults to removing the last character.
+        # When the original string is empty, returns an empty string
+        def but_last(num_of_characters = 1)
+          self[0..-(1 + num_of_characters)]
         end
       end
     end
--- spec/ruby_on_asteroids/core_ext/string/accessing_spec.rb
+++ spec/ruby_on_asteroids/core_ext/string/accessing_spec.rb
@@ -6,21 +6,43 @@ RSpec.describe "String extensions to improve the way we access to the string con
   String.include RubyOnAsteroids::CoreExt::String::Accessing
 
   describe "#but_last" do
-    context "when the String is longer than 1 character" do
-      it "returns a String without the last character" do
-        expect("house".but_last).to eq "hous"
+    describe "without passing a number of characters to be removed" do
+      context "when the string is longer than 1 character" do
+        it "returns a String without the last character" do
+          expect("house".but_last).to eq "hous"
+        end
+      end
+
+      context "when the string has 1 character" do
+        it "returns an empty String" do
+          expect("h".but_last).to eq ""
+        end
+      end
+
+      context "when the string is empty" do
+        it "returns an empty String" do
+          expect("".but_last).to eq ""
+        end
       end
     end
 
-    context "when the String has 1 character" do
-      it "returns an empty String" do
-        expect("h".but_last).to eq ""
+    describe "passing a number of characters to be removed" do
+      context "when the string has enough characters to be removed" do
+        it "returns a copy of the String after removing from the end the amount of characters requested" do
+          expect("house".but_last(2)).to eq "hou"
+        end
       end
-    end
 
-    context "when the String is empty" do
-      it "returns an empty String" do
-        expect("".but_last).to eq ""
+      context "when the string has exactly the same number of characters to be removed" do
+        it "returns an empty string" do
+          expect("car".but_last(3)).to eq ""
+        end
+      end
+
+      context "when the string is shorter than the number of characters to be removed" do
+        it "returns an empty string" do
+          expect("car".but_last(5)).to eq ""
+        end
       end
     end
   end
